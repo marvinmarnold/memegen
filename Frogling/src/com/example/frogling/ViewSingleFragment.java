1ece3b3f4a82cd451b37d47c5a9b499c9bf33d41
Zack Wari <Zakariya@nwcse-105.cs.huji.ac.il>
2013-07-02 18:35:41 +0300

    Present the timestamp of the submitted meme in the explore view. Fix a bug in landscape mode where the uploaded image would not be preserved in create mode

--- Frogling/src/com/example/frogling/ViewSingleFragment.java
+++ Frogling/src/com/example/frogling/ViewSingleFragment.java
@@ -3,6 +3,12 @@
  */
 package com.example.frogling;
 
+import java.security.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 import utilities.BackEnd;
 import android.app.Activity;
 import android.app.Fragment;
@@ -11,13 +17,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 
@@ -33,15 +42,16 @@ public class ViewSingleFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		return inflater.inflate(R.layout.view_single_meme, container, false);
 	}
 
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		setHasOptionsMenu(true);
-		
-		Button nextButton = (Button) getActivity().findViewById(R.id.view_next_button);
+
+		Button nextButton = (Button) getActivity().findViewById(
+				R.id.view_next_button);
 		nextButton.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -52,12 +62,20 @@ public class ViewSingleFragment extends Fragment {
 		});
 	}
 
+	/**
+	 * 
+	 * @param unix_timestamp
+	 * @return
+	 * @throws ParseException
+	 */
+
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		super.onCreateOptionsMenu(menu, inflater);
-		// MenuItem upload_item = menu.findItem(R.id.action_upload);
-		// upload_item.setVisible(false);
+		MenuItem upload_item = menu.findItem(R.id.action_upload);
+		upload_item.setVisible(false);
+		MenuItem post_item = menu.findItem(R.id.action_save_to_parse);
+		post_item.setVisible(false);
 	}
-
 
 	/**
 	 * Shows the next meme from the meme query, uses the BackEnd.getNextMeme()
@@ -65,6 +83,7 @@ public class ViewSingleFragment extends Fragment {
 	 */
 	public void showNextMeme() {
 		ParseObject object = BackEnd.getNextMeme();
+
 		if (object != null) {
 			ImageView viewedImage = (ImageView) getActivity().findViewById(
 					R.id.view_meme_image);
@@ -72,15 +91,22 @@ public class ViewSingleFragment extends Fragment {
 					R.id.view_top_text);
 			TextView viewedBottomText = (TextView) getActivity().findViewById(
 					R.id.view_bottom_text);
+			TextView createdTime = (TextView) getActivity().findViewById(
+					R.id.time_stamp);
 			try {
 				Bitmap map = BackEnd.convertByteToBit(((ParseFile) object
 						.get(BackEnd.IMAGE_KEY)).getData());
 				String topText = (String) object.get(BackEnd.TOP_TEXT);
 				String bottomText = (String) object.get(BackEnd.BOTTOM_TEXT);
+				long timeInMilli = (Long) object.get(BackEnd.TIME_STAMP);
+
+				// Toast.makeText(getActivity(), unixToDate(timeInMilli),
+				// Toast.LENGTH_LONG).show();
 
 				viewedImage.setImageBitmap(map);
 				viewedTopText.setText(topText);
 				viewedBottomText.setText(bottomText);
+				createdTime.setText(unixToDate(timeInMilli));
 
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
@@ -89,5 +115,14 @@ public class ViewSingleFragment extends Fragment {
 		} else {
 
 		}
+	}
+
+	public String unixToDate(long timestamp_in_mill) {
+		Date date = new Date(timestamp_in_mill);
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		df.setTimeZone(TimeZone.getDefault());
+		String dateStr = df.format(date);
+
+		return dateStr;
 	}
 }
